48b68f418f301ea11505d939eb0ab23add7c8f2e
bjarte1990 <csaba.pigler@gmail.com>
2016-05-04 12:57:37 +0200

    Update locations.py

--- locations.py
+++ locations.py
@@ -30,49 +30,63 @@ def twitterLocation(content, tweetNum=10):
 			
 
 	return locationList
+ 
+def get_locations_by_content(content, tweetNum=10):
+    
+    tweetJson=twitterHandler.getTweetsByContent(content, tweetNum)
+    statuses=tweetJson['statuses']
 
-def getLatLong(addresses):
-	#Retrievs geocodes of given addresses using google maps API
-	locationData=[]
-	i=0
-	for a in addresses:
-		try:
-			i=i+1
-			if i==9:
-				i=0
-				time.sleep(10)
-			url=GOOGLEMAPS_URL+urllib.urlencode({'sensor':'false', 'address': a})
-			#print 'Retrieving ',url
-			html=urllib.urlopen(url).read()
+    locationList=[]
+    for tweet in statuses:
+        try:
+            location=tweet['user']['location'];
+            if location:
+                locationList.append(location)
+        except UnicodeEncodeError:
+            continue
+    
+    return locationList
 
-			#print 'Retrieved ',len(html),' characters'
-
-			info=json.loads(html)
-
-			if 'status' not in info or info['status'] != 'OK':
-				print '==== Failure To Retrieve ===='
-				print html
-			else:
-				data= info['results'][0]
-				
-				address=data['formatted_address']
-					
-				print 'Address: ',address
-				
-				location=data['geometry']['location']
-
-				print 'Lat: ',location['lat']," Long: ",location['lng']
-				
-				newData=dict()
-				newData['name']=address
-				newData['lat']=location['lat']
-				newData['long']=location['lng']
-				locationData.append(newData)
-		except UnicodeEncodeError:
-			print 'Encoding error...'
-			
-	return locationData
-	
+def get_lat_long(addresses):
+    #Retrievs geocodes of given addresses using google maps API
+    locationData=[]
+    i=0
+    for a in addresses:
+        try:
+            i=i+1
+            if i==9:
+                i=0
+                time.sleep(10)
+            url=GOOGLEMAPS_URL+urllib.urlencode({'sensor':'false', 'address': a})
+    		#print 'Retrieving ',url
+            html=urllib.urlopen(url).read()
+    
+            #print 'Retrieved ',len(html),' characters'
+            info=json.loads(html)
+    
+            if 'status' not in info or info['status'] != 'OK':
+                
+                print '==== Failure To Retrieve ===='
+                print html
+            else:
+                data = info['results'][0]
+    				
+                address=data['formatted_address']
+    					
+                print 'Address: ',address
+    				
+                location=data['geometry']['location']
+    
+                print 'Lat: ',location['lat']," Long: ",location['lng']
+                newData=dict()
+                newData['name']=address
+                newData['lat']=location['lat']
+                newData['long']=location['lng']
+                locationData.append(newData)
+        except UnicodeEncodeError:
+            print 'Encoding error...'
+    			
+    return locationData	
 #def loadData():
 #	with open('geoData.json') as dataFile:
 #		data=json.load(dataFile)
